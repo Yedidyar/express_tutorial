dd2ea1763851440d5c66fe6847caebdceeaefe57
yedidyar <yedidyarashi@gmail.com>
2021-09-13 17:03:59 +0300

    started tutorial

---
+++ Members.js
@@ -0,0 +1,7 @@
+const members = [
+  { id: 1, name: "moshe", email: "moshe@gmail.com", status: "active" },
+  { id: 2, name: "mosh", email: "mosh@gmail.com", status: "active" },
+  { id: 3, name: "david", email: "david@gmail.com", status: "disabeled" },
+];
+
+module.exports = members;
---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+const members = require("./Members");
+const logger = require("./middleware/logger");
+const app = express();
+
+// app.use(logger);
+
+app.get("/api/members", (req, res) => {
+  res.json(members);
+});
+// Set a static folder
+
+app.get("/api/members/:id", (req, res) => {
+  const found = members.some((member) => member.id === parseInt(req.params.id));
+  if (found) {
+    res.json(members.filter((member) => member.id === parseInt(req.params.id)));
+  } else {
+    res.status(400).json({ msg: `Member with the id of ${req.params.id}` });
+  }
+});
+
+// app.use(express.static(path.join(__dirname,"public")))
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`server started on port: http://localhost:${PORT}`)
+);
